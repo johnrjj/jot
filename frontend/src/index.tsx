bf2da55cc9c1482b0ef43cbf6fcd07ca488819a2
John Johnson <johnrjj@gmail.com>
2018-11-03 10:14:16 -0700

    build problems

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './App';
 
 const API_ENDPOINT_ROOT =
   process.env.NODE_ENV === 'development'
