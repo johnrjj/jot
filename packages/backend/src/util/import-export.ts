b504c0e8d526e6523081c6f9bc788cccf6d445ac
John Johnson <johnrjj@gmail.com>
2018-11-25 07:33:06 -0500

    Renaming constant to uppercase

--- packages/backend/src/util/import-export.ts
+++ packages/backend/src/util/import-export.ts
@@ -1,24 +1,21 @@
 import Automerge from 'automerge';
-import { Value } from 'slate';
 import Plain from 'slate-plain-serializer';
 import uuid from 'uuid/v4';
 
-const serverGeneratedActorId = '000000000000';
+const SYSTEM_GENERATED_ACTOR_ID = '000000000000';
+const IPSUM_PARAGRAPH_TEXT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac ligula justo. Proin mollis, neque eu aliquam bibendum, nisi ligula consequat felis, at commodo nulla sapien in augue. Mauris in dignissim purus. Morbi sodales massa elit, sit amet varius tellus interdum nec. Maecenas consequat, ante eget facilisis cursus, elit quam condimentum neque, ut hendrerit neque massa vitae urna. Donec suscipit leo eros, et convallis nisl gravida ut. Nam at dolor neque. Vivamus egestas sit amet sapien eget molestie. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus varius tellus nulla, at luctus enim consectetur in. Proin placerat magna in velit eleifend, ac consectetur diam tincidunt. Proin augue augue, pellentesque sed tristique sodales, tristique quis metus.`;
 
-import { initialAutomergeDocExample } from '../test-data/initial-doc';
 import { slateCustomToJson } from '@jot/common/dist/adapter/slate-automerge-bridge';
 
 const serializePlainTextToSlateValue = (text: string, toJSON: boolean = false) => Plain.deserialize(ipsum, { toJSON });
 
 const createAutomergeDocFromJson = (json: any): Automerge.AutomergeRoot => {
-  const blankDoc = Automerge.init(serverGeneratedActorId);
+  const blankDoc = Automerge.init(SYSTEM_GENERATED_ACTOR_ID);
   const hydratedDoc = Automerge.change(blankDoc, 'Hydrated Document with initial data', doc => {
-    // Object.assign(doc, json);
     Object.keys(json).forEach(key => {
       doc[key] = json[key];
     });
   });
-  console.log(Automerge.save(hydratedDoc));
   return hydratedDoc;
 };
 
@@ -34,17 +31,14 @@ const generateAutomergeDoc = (
   docId: string = uuid(),
 ): { doc: Automerge.AutomergeRoot; docId: string } => {
   const automergeDoc = buildAutomergeDocState(initialDocText);
-  console.log(automergeDoc);
   return {
     doc: automergeDoc,
     docId: docId,
   };
 };
 
-const ipsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac ligula justo. Proin mollis, neque eu aliquam bibendum, nisi ligula consequat felis, at commodo nulla sapien in augue. Mauris in dignissim purus. Morbi sodales massa elit, sit amet varius tellus interdum nec. Maecenas consequat, ante eget facilisis cursus, elit quam condimentum neque, ut hendrerit neque massa vitae urna. Donec suscipit leo eros, et convallis nisl gravida ut. Nam at dolor neque. Vivamus egestas sit amet sapien eget molestie. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus varius tellus nulla, at luctus enim consectetur in. Proin placerat magna in velit eleifend, ac consectetur diam tincidunt. Proin augue augue, pellentesque sed tristique sodales, tristique quis metus.`;
-
-const generateSampleAutomergeDocFromIpsum = (docId?: string) => {
-  return generateAutomergeDoc(ipsum, docId);
+const generateSampleAutomergeDocFromIpsum = (docIdToAssign?: string) => {
+  return generateAutomergeDoc(IPSUM_PARAGRAPH_TEXT, docIdToAssign);
 };
 
 export { generateAutomergeDoc, generateSampleAutomergeDocFromIpsum };
