4110955ff4659f86b28b51a61761c1b94782275e
John Johnson <johnrjj@gmail.com>
2018-12-02 14:28:56 -0800

    Right to left and left to right remote cursors working, still needs styling and cleanup (#7)

--- packages/frontend/src/components/Cursor.tsx
+++ packages/frontend/src/components/Cursor.tsx
@@ -3,32 +3,64 @@ import styled, { keyframes } from 'styled-components';
 import { lighten, desaturate, opacify, transparentize, getLuminance, darken } from 'polished';
 
 const blink = keyframes`
-    0% { opacity:1; }
-    49% { opacity:1; }
-    50% { opacity:0; }
-    99% { opacity:0; }
-    100% { opacity:1; }
+  0% { opacity:1; }
+  49% { opacity:1; }
+  50% { opacity:0; }
+  99% { opacity:0; }
+  100% { opacity:1; }
+`;
+
+const popUpwards = keyframes`
+  0% { 
+    opacity:0;
+    transform: matrix(.97, 0, 0, 1, 0, 12); 
+  }
+  20% { 
+    opacity:0.7; 
+    transform: matrix(.99, 0, 0, 1, 0, 2);
+  }
+  40% { 
+    opacity:1;
+    transform: matrix(1, 0, 0, 1, 0, -1); 
+  }
+  70% { 
+    opacity:1;
+    transform: matrix(1, 0, 0, 1, 0, 0); 
+  }
+  100% { 
+    opacity:1;
+    transform: matrix(1, 0, 0, 1, 0, 0); 
+  }
+`;
+
+const ActiveMenu = styled.div`
+  background-color: black;
+  display: inline-block;
+  visibility: visible;
+  transition: top 75ms ease-out, left 75ms ease-out;
+  animation: ${popUpwards} 180ms linear;
 `;
 
 const RemoteCursorRangeMark = styled.div`
-  overflow: hidden;
+  /* overflow: hidden; */
   position: absolute;
+  min-width: 2px;
   width: 2px;
-  height: 100%;
+  /* height: 100%; */
   top: 0;
-  left: ${props => (props.isCollapsedAtEnd ? '100%' : 0)};
+  left: ${props => (props.isCollapsedAtEnd ? '100%' : `calc(0px - 1px)`)};
   bottom: 0;
-  background-color: ${props => darken(0.3, props.markerColor)};
+  background-color: ${props => darken(0.2, props.markerColor)};
 `;
 
 const SpanRelativeAnchor = styled.span`
   position: relative;
+  pointer-events: none;
 `;
 
 const SpanRelativeAnchorWithBackgroundColor = styled.span`
   position: relative;
   pointer-events: none;
-  line-height: 2;
   background-color: ${props => transparentize(0.3, props.markerColor)};
 `;
 
@@ -39,6 +71,7 @@ const AbsoluteFullWidth = styled.div`
   right: 0;
   bottom: 0;
   user-select: none;
+  min-width: 2px;
 `;
 
 const MARKER_HEIGHT = 12;
@@ -47,14 +80,11 @@ const CursorMarker = styled.span`
   position: absolute;
   width: ${MARKET_WIDTH};
   height: ${MARKER_HEIGHT};
-  top: -${MARKER_HEIGHT / 2};
-  left: -${MARKET_WIDTH / 2};
+  top: 0;
+  left: ${props => (props.isCollapsedAtEnd ? '100%' : 0)};
+
   background-color: ${props => darken(0.2, props.markerColor)};
 `;
-
-const RemoteCursorCollaprsedMark = ({ children, ...rest }) => {
-  return <div>{children}</div>;
-};
 
 export {
   CursorMarker,
@@ -62,4 +92,5 @@ export {
   AbsoluteFullWidth,
   RemoteCursorRangeMark,
   SpanRelativeAnchorWithBackgroundColor,
+  ActiveMenu,
 };
